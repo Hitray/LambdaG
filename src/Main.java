21efd2f303b7ab604dd1ab1028bd183afee94ace
Hitray <75577433+Hitray@users.noreply.github.com>
2022-09-03 23:54:31 +0500

    Branch Eraser for PR

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Main {
     private static final int MAXIMUM_AGE = 99;
+    private static final int MINIMUM_AGE = 18;
 
     public static void main(String[] args) {
         Random random = new Random();
@@ -15,23 +16,15 @@ public class Main {
                 new Person("Jack", "Гений", random.nextInt(MAXIMUM_AGE) + 1)
         };
 
-        List<Person> peoplesList = new ArrayList<>(List.of(someGuys));
-        peoplesList.sort(new PeopleComparator());
-
-
-        TreeSet<Person> ts = new TreeSet<>(new PeopleComparator());
-        ts.addAll(List.of(someGuys));
+             List<Person> peoplesList = new ArrayList<>(List.of(someGuys));
+        peoplesList.removeIf(person -> person.getAge() < MINIMUM_AGE);
 
         System.out.print("\nСписок на входе:\n");
-        for (Person person: someGuys) {
+        for (Person person : someGuys) {
             System.out.printf("Фамилия: %-25s, Возраст: %3d\n", person.getSurname(), person.getAge());
         }
         System.out.print("\nСписок List:\n");
-        for (Person person: peoplesList) {
-            System.out.printf("Фамилия: %-25s, Возраст: %3d\n", person.getSurname(), person.getAge());
-        }
-        System.out.print("\nСписок TreeSet:\n");
-        for (Person person: ts) {
+        for (Person person : peoplesList) {
             System.out.printf("Фамилия: %-25s, Возраст: %3d\n", person.getSurname(), person.getAge());
         }
     }
